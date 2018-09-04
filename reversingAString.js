01317eef8216ad9c2e0b950b2196d8fa6613e141
Hakki Suman <sumanhakki@gmail.com>
2018-09-04 13:24:10 -0600

    Added another solution using a for loop

--- reversingAString.js
+++ reversingAString.js
@@ -1,8 +1,19 @@
-//Reverse a given String
+//Reverse a given String using string and array methods
 let reverseString = word => {
-    let reversedString = word.split('');
-    return reversedString.reverse();
+    let reversedString = word.split('').reverse().join('');
+    return reversedString;
 
 }
 
 console.log(reverseString('word'));
+
+// Reverse a given String using a for loop
+let reverseString2 = word => {
+    let reversedWord = '';
+    for (i = word.length - 1; i >= 0; i--)
+        reversedWord += word[i];
+    return reversedWord;
+
+}
+
+console.log(reverseString2('Hakki'))
--- reversingAStringREADME.md
+++ reversingAStringREADME.md
@@ -1,4 +1,7 @@
 # Reverse A String
-    Reverse any given string by using String split() and Array reverse() methods
-        * split() gives us a character array of a string
-        * reverse()
+
+    Reverse any given string by using String split() join() and Array reverse() methods, and a for loop.
+        - split() gives us a character array of a string
+        - reverse() reverses the array
+        - join() restores array back to a string
+        - last method using a for loop is the cleaner code because the code has     to iterate through the word being passed in only once.
