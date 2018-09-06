0ec6eba29f0b0e6aea43983143ffcdba31382b24
Hakki Suman <sumanhakki@gmail.com>
2018-09-05 23:05:39 -0600

    Initial Commit

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+//Checking for whether a given string is a Palindrome
+function isPalindrome(str) {
+    let forwrdStr = str.toLowerCase().replace(/[\W_]/g, '');
+    let reverseStr = forwrdStr.split('').reverse().join('');
+    return forwrdStr === reverseStr
+}
+
+
+
+console.log(isPalindrome("A man, a plan, a canal. Panama"));
+console.log(isPalindrome("My age is 0, 0 si ega ym."));
+console.log(isPalindrome("_codgod"));
+console.log(isPalindrome("legandary"));
+console.log(isPalindrome(".0_0 (: /-\ :) 0-0"));
---
+++ palindromeREADME.md
@@ -0,0 +1,6 @@
+# Palindrome
+
+    This algorithm is to determine whether a given string is a Palindrome
+    - Fisrt we strip all the spaces, periods, and underscores that gives us the pure string value.
+    - Second, we store reverse string in reverseStr variable using String and Array methods
+    - Third, we check to see if both variables are equal.
